5916b181bc209855f78bec5b142dec2dc992a371
Mohamedbakr22 <mohammedfawzy0210@gmail.com>
2023-08-14 10:17:58 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,34 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ *Return: 0 (success)
+*/
 int main(void)
-{
-	int n,m;
 
-	for (n =48; n<= 56;n++)
+{
+
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
 	{
-		for (m=49;m <=57;m++)
-		{
-			if (m > n)
-			{
-				putchar(n);
-	                        putchar(m);
-				if (n !=56 || m != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
+	for (m = 49; m <= 57; m++)
+	{
+	if (m > n)
+	{
+	putchar(n);
+	putchar(m);
+	if (n != 56 || m != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
