a94103a641d7881cf3f23fd510f39ce688731ceb
Mohamedbakr22 <mohammedfawzy0210@gmail.com>
2023-08-14 09:00:19 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ *Return: 0 (success)
+*/
 int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d",i);
+		printf("%d", i);
 	}
-	putchar("\n");
+	printf("\n");
+
 	return (0);
 }
