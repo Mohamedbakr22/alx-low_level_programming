f2507037dac42e9f51b8b619a63ce21933070bd8
Mohamedbakr22 <mohammedfawzy0210@gmail.com>
2023-08-14 09:49:08 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ *Return: 0 (success)
+*/
 int main(void)
+
 {
-        int i;
-        for (i = 48;i > 58;i++)
-        {
-                putchar(i);
-        }
-	 for (i = 97;i > 103; i++)
-        {
-                putchar(i);
-        }
-        putchar("\n");
-        return (0);
+
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+
+	for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
 }
